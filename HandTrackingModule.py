cebaa0d1bc4f293494a94435710b5b07b945e43f
revaldift1 <106583915+revaldift1@users.noreply.github.com>
2022-05-31 18:45:15 +0700

    Update HandTrackingModule.py

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -14,3 +14,15 @@ class handDetector():
         self.mpHands = mp.solutions.hands
         self.hands = self.mpHands.Hands(self.mode,self.maxHands,self.modelComplex,self.detectionCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
+        
+    def findHands(self, img, draw = True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        #print(results.multi_hand_landmarks)
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+                
+        return img
