60e10217f9fad8eacffca4ec2b687458456c17ff
Rapehaja <100468441+Rapehaja@users.noreply.github.com>
2022-05-31 19:28:04 +0700

    Update HandTrackingModule.py

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -26,3 +26,19 @@ class handDetector():
                     self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
                 
         return img
+
+    def findPosition(self, img, handNo=0, draw = True): 
+
+        lmList = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                #print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                #print(id, cx, cy)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 15, (255,0,255), cv2.FILLED)
+
+        return lmList
